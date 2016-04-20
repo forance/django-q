804af0aca495e80df6f56fcce272c1481ff8c884
forance <forance@users.noreply.github.com>
2016-04-20 22:50:51 +0800

    Delete import_csv.sql

--- import_csv.sql
+++
@@ -1,2 +0,0 @@
-COPY order_reminder_Orders(order_id,ship_date,customer,currency,creater,order_amount,create_date) 
-FROM '/Users/nicktang/Documents/2016Talk-Django-Q-master/sc_simple.csv' DELIMITER ',' CSV HEADER;
